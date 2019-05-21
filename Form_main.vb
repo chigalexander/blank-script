6355d3f23d58ea955c1900e5bcf2352d870cf874
chigalexander <50860460+chigalexander@users.noreply.github.com>
2019-05-21 12:29:02 +0500

    Add files via upload

---
+++ Form_main.vb
@@ -0,0 +1,16 @@
+﻿Imports Illustrator
+Public Class Form_main
+    Dim ill As Illustrator.Application
+    Dim doc As Illustrator.Document
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ill = CreateObject("Illustrator.Application.CC.2019")
+        draw()
+        End
+    End Sub
+    Private Sub draw()
+        Dim comStr As String
+        'Try
+        comStr = Command$()
+        ill.SendScriptMessage("Alliance Blank", "blank", comStr)
+    End Sub
+End Class
